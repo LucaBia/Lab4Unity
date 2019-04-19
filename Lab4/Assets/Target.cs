458240f2dc6a6f8d05b1ec94caeb5a5d7e878389
Gian Luca Rivera <gian.luca.99@hotmail.com>
2019-04-18 21:37:13 -0600

    Disparo

---
+++ Lab4/Assets/Target.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public float health = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}

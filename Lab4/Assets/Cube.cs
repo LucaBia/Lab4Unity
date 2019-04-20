486fb299c8be7be4ca2605affef88411e564896b
Gian Luca Rivera <gian.luca.99@hotmail.com>
2019-04-19 20:51:11 -0600

    Fuerza

--- Lab4/Assets/Cube.cs
+++ Lab4/Assets/Cube.cs
@@ -6,11 +6,14 @@ public class Cube : MonoBehaviour
 {
 
     public Color colorChange;
+    private Rigidbody rBody;
+
+    public float force = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -27,5 +30,14 @@ public class Cube : MonoBehaviour
     private void OnMouseExit()
     {
         gameObject.GetComponent<MeshRenderer>().material.color = Color.gray;
+    }
+
+    private void OnMouseDown()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(myRay, out hitInfo))
+            rBody.AddForce(-hitInfo.normal * force, ForceMode.Impulse);
     }
 }
--- Lab4/Assets/Sphere.cs
+++ Lab4/Assets/Sphere.cs
@@ -6,11 +6,14 @@ public class Sphere : MonoBehaviour
 {
 
     public Color colorChange;
+    private Rigidbody rBody;
+
+    public float force = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,15 @@ public class Sphere : MonoBehaviour
     private void OnMouseExit()
     {
         gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    private void OnMouseDown()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(myRay, out hitInfo))
+            rBody.AddForce(-hitInfo.normal * force, ForceMode.Impulse);
     }
 
 }
